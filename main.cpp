d6a4962246c468863c76c1e3118588237154366e
anyable <stupiranna@mail.ru>
2025-11-07 12:22:26 +0300

    extend

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+int * extend(const int * a, size_t k, size_t d, int filler)
+{
+  int * new_array = new int[d];
+  for (size_t i = 0; i < std::min(k, d); i++)
+  {
+    new_array[i] = a[i];
+  }
+  for (size_t i = std::min(k, d); i < d; i++)
+  {
+    new_array[i] = filler;
+  }
+  return new_array;
+}
+
+int main()
+{
+  int array[5] = {1, 2, 3, 4, 4};
+  int * new_array = extend(array, 5, 10, 0);
+  for (size_t i = 0; i < 10; i++)
+  {
+    std::cout << new_array[i];
+  }
+  std::cout << "\n";
+}
