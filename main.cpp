3c44ad9034c3ba93977fa5af750e6dada5bf4e2d
anyable <stupiranna@mail.ru>
2025-11-07 12:57:59 +0300

    add row

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 int * extend(const int * a, size_t k, size_t d, int filler);
 
+int *add_row(const int * a, size_t n, size_t m, int filler)
+{
+  return extend(a, n * m, (n + 1) * m, filler);
+}
+
+
 void extend(int ** ptr_a, size_t k, size_t d, int filler)
 {
   int * new_array = extend(*ptr_a, k, d, filler);
@@ -25,10 +31,14 @@ int * extend(const int * a, size_t k, size_t d, int filler)
 int main()
 {
   int * array = new int[5]{1, 2, 3, 4, 5};
-  int **ptr_a = &array;
-  extend(ptr_a, 5, 10, 0);
-  for (size_t i = 0; i < 10; i++)
+  int * new_matrix = add_row(array, 2, 2, 0);
+  for (size_t i = 0; i < 3; i++)
   {
-    std::cout << array[i];
+    for (size_t j = 0; j < 2; j++)
+    {
+      std::cout << new_matrix[i * 2 + j] << ' ';
+    }
+    std::cout << "\n";
   }
+  std::cout << "\n";
 }
